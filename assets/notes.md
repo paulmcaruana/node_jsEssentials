3bb3758197827c60774f081b8db9108ded21e00c
Paul Caruana <paul@beambient.co.uk>
2020-08-25 12:08:04 +0100

    updated examples of file system basics

--- assets/notes.md
+++
@@ -1,7 +0,0 @@
-#this is a new file
-
-We can write text to a file with the fs,writeFile
-
-*fs.readdir 
-*fs.readFile 
-*fs.writeFile
---
+++ directories.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+// fs.renameSync("./storage-files", "./storage");
+
+fs.readdirSync("./storage").forEach(fileName => {
+    fs.unlinkSync(`./storage/${fileName}`);
+});
+
+fs.rmdir("./storage", err => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("./storage directory removed");
+
+});
